3d01a9eb96300a04a02c9357ad9cc0c45bd9bbbb
theborgh <borghiborghi@gmail.com>
2019-07-11 21:10:56 +0200

    Add card background images and more styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,10 @@ import './Card.scss';
 
 const card = (props) => {
    return (
-      <div className="Card">
+      <div className={`${props.size} Card`}
+         style={{
+            backgroundImage: `url(${props.imageUrl})`
+         }}>
          <div className="hp-card-content">
             <h1 className="hp-card-title">{props.item}</h1>
             <span className="hp-card-subtitle">Shop now</span>
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
+import './CardContainer.scss'
 import Card from '../Card/Card'
 
 class CardContainer extends Component {
    constructor() {
       super();
       this.state = {
-         cardTitles: ['Hats', 'Jackets', 'Shoes', 'Womens', 'Mens']
+         sections: [
+            {
+               title: 'hats',
+               imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+               id: 1,
+               linkUrl: 'shop/hats'
+            },
+            {
+               title: 'jackets',
+               imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+               id: 2,
+               linkUrl: 'shop/jackets'
+            },
+            {
+               title: 'shoes',
+               imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+               id: 3,
+               linkUrl: 'shop/shoes'
+            },
+            {
+               title: 'womens',
+               imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+               size: 'large',
+               id: 4,
+               linkUrl: 'shop/womens'
+            },
+            {
+               title: 'mens',
+               imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+               size: 'large',
+               id: 5,
+               linkUrl: 'shop/mens'
+            }
+         ]
       }
    }
 
    render() {
 
-      return(
+      return (
          <div className="CardContainer">
-            {this.state.cardTitles.map(title => <Card key={title} item={title} />)}
+            {this.state.sections.map(sect => <Card key={sect.id} 
+                                                   item={sect.title} 
+                                                   imageUrl={sect.imageUrl}
+                                                   linkUrl={sect.linkUrl}
+                                                   size={sect.size}  />)}
          </div>
       );
    }
