c55bb77579b87a3467b11f858e1c256cdd1b1bee
theborgh <borghiborghi@gmail.com>
2019-07-11 21:41:46 +0200

    Add card animations

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,12 +3,13 @@ import './Card.scss';
 
 const card = (props) => {
    return (
-      <div className={`${props.size} Card`}
-         style={{
+      <div className={`${props.size} Card`}>
+         <div className="background-image" style={{
             backgroundImage: `url(${props.imageUrl})`
          }}>
+         </div>
          <div className="hp-card-content">
-            <h1 className="hp-card-title">{props.item}</h1>
+            <h1 className="hp-card-title">{props.item.toUpperCase()}</h1>
             <span className="hp-card-subtitle">Shop now</span>
          </div>
       </div>
